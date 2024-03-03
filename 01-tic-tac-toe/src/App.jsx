97771450ef2c53a8c68f79314bbc0b52172b7d49
Sergio Gutiérrez <sergio.gutierrez@innocv.com>
2024-03-03 09:27:45 +0100

    Extracting const's that must be global to a utils folder.

--- 01-tic-tac-toe/src/App.jsx
+++ 01-tic-tac-toe/src/App.jsx
@@ -1,23 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react'
 import { Square } from './components/Square'
+import { TURNS, WINNER_COMBOS } from './utils/GlobalConst'
 import './App.css'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
